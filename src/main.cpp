e7dfa276b9131bbd4f96fba45b1ebbc2d4990379
Louis <louis.rigal4@gmail.com>
2022-07-07 23:29:55 +0200

    full workflow

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <fstream>
 #include <filesystem>
+
+#include <opencv2/opencv.hpp>
+
+#include <nlohmann/json.hpp>
+
+#include "bulleye/Bulleye.h"
+#include "bulleye/BulleyeDisplay.h"
 
 int main(int argc, char** argv)
 {
@@ -14,7 +22,51 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
 
-    // TODO
+    std::string path = argv[1];
+    nlohmann::json j;
+    try {
+        std::ifstream i(path);
+        i >> j;
+    }
+    catch (const std::exception& e) {
+        std::cout << "Unable to parse file: " << e.what() << std::endl;
+    }
+
+    auto apex = j.at("apex").get<std::vector<double>>();
+    double apex_arr[] = {apex[0], apex[1], apex[2]};
+    auto base = j.at("base").get<std::vector<double>>();
+    double base_arr[] = {base[0], base[1], base[2]};
+    auto front = j.at("front").get<std::vector<double>>();
+    double front_arr[] = {front[0], front[1], front[2]};
+    auto coords = j.at("coords").get<std::vector<std::vector<double>>>();
+    auto values = j.at("values").get<std::vector<double>>();
+    auto mm = std::minmax_element(values.begin(), values.end());
+    auto min = *mm.first;
+    auto max = *mm.second;
+
+    auto config = BulleyeConfig::bulleye_18_segments;
+    auto be = std::make_shared<Bulleye>(config, apex_arr, base_arr, front_arr);
+
+    std::vector<std::tuple<double, double, double>> coords_scalars;
+    for (const auto& c: coords) {
+        coords_scalars.emplace_back(std::make_tuple(c[0], c[1], c[2]));
+    }
+    be->add_scalars("Values", coords_scalars, values);
+
+    BulleyeDisplay disp(be);
+
+    auto lut = vtkSmartPointer<vtkLookupTable>::New();
+    lut->SetTableRange(0., 10.);
+    lut->SetHueRange(0, 0.85);
+    lut->SetSaturationRange(1, 1);
+    lut->SetValueRange(1, 1);
+    lut->SetNanColor(0.8, 0.8, 0.8, 1);
+
+    auto mat = disp.draw("Values", lut);
+
+    cv::flip(mat, mat, 0);
+    cv::imshow("Bullseye", mat);
+    cv::waitKey();
 
     return EXIT_SUCCESS;
 }
